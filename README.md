140d85a77ace9d1932eff6739261a5ba4b5545e8
Yang-heesu <hsge0602@gmail.com>
2023-02-22 23:56:37 +0900

    location을 객체타입으로 수정

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # useMemo 사용해보기
 
 useMemo를 이용한 코드 작성
+
+참고 영상 : https://www.youtube.com/watch?v=e-CnI8Q5RY4&t=1s
--- src/Travel.js
+++ src/Travel.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Travel() {
   const [number, setNumber] = useState(0);
   const [isKorea, setIsKorea] = useState(true);
 
-  const location = isKorea ? '한국' : '외국';
+  // const location = isKorea ? '한국' : '외국';
+
+  const location = {
+    country : isKorea ? '한국' : '외국'
+  }
+
+  //location이 원시타입일 때는 비행기 타자 버튼을 누를 때만 호출된다.
+  //객체타입일 때는 렌더링될 때마다 변수에 담긴 객체의 주소가 바뀌기 때문에
+  //비행기 타자 버튼을 누를 때만 호출되는 것이 아니라 다른 값을 변화시켜도 호출된다. 
+  useEffect(() => {
+    console.log("useEffect 호출");
+  }, [location]);
 
   return (
     <div>
@@ -16,7 +27,7 @@ function Travel() {
       />
       <hr/>
       <h2>어느 나라에 있어요?</h2>
-      <p>나라 : {location}</p>
+      <p>나라 : {location.country}</p>
       <button onClick={() => setIsKorea(!isKorea)}>비행기 타자~</button>
     </div>
   );
