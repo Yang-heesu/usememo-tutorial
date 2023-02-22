1b09c5fd3495ae132339e455cae9583c97096561
Yang-heesu <hsge0602@gmail.com>
2023-02-22 22:36:39 +0900

    쉬운 계산기 추가

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ const hardCalculate = (number) => {
   return number + 10000;
 }
 
+const easyCalculate = (number) => {
+  console.log("쉬운 계산");
+  return number + 1;
+}
+
 function App() {
   const [hardNumber, setHardNumber] = useState(1);
+  const [easyNumber, setEasyNumber] = useState(1);
 
   //함수형 컴포넌트이기 때문에 hardNumber이 바뀔 때마다 렌더링이 된다.
   //hardSum이 초기화되어 hardCalculate함수를 실행하고 값을 받아오는데 시간이 걸린다.
   //어려운 계산기의 input number를 수정한 후 hardSum 값이 바뀌는 데에 딜레이가 있다.
   const hardSum = hardCalculate(hardNumber);
+
+  //for문을 없앴지만 hardSum처럼 딜레이가 있다.
+  //함수형 컴포넌트라서 easyNumber를 바꾸면 hardSum, easySum 둘 다 초기화가 된다.
+  const easySum = easyCalculate(easyNumber);
 
   return (
     <div>
@@ -23,6 +33,14 @@ function App() {
         onChange={(e) => setHardNumber(parseInt(e.target.value))}
       />
       <span> + 10000 = {hardSum}</span>
+
+      <h3>쉬운 계산기</h3>
+      <input
+        type="number"
+        value={easyNumber}
+        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
+      />
+      <span> + 1 = {easySum}</span>
     </div>
   );
 }
